7180fd85e92accd345f5d47419634eb93bd8f1ee
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-12-05 12:13:15 +0200

    commit 002

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
+
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Not found" })
 });
+
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
